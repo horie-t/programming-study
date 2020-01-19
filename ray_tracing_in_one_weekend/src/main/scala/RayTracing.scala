f2bca194f32ee071c97b366ced409bb847b8afb1
HORIE Tetsuya <horie-t@users.noreply.github.com>
2020-01-19 17:41:06 +0900

    RayTracing: 004 Rays, a Simple Camara, and Background

--- ray_tracing_in_one_weekend/src/main/scala/RayTracing.scala
+++ ray_tracing_in_one_weekend/src/main/scala/RayTracing.scala
@@ -78,6 +78,8 @@ object Vec3 {
     def *(v: Vec3): Vec3 = new Vec3(self * v(0), self * v(1), self * v(2))
   }
 
+  def apply(e0: Float, e1: Float, e2: Float): Vec3 = new Vec3(e0, e1, e2)
+
   def dot(v1: Vec3, v2: Vec3): Float = v1(0) * v2(0) + v1(1) * v2(1) + v1(2) * v2(2)
   def cross(v1: Vec3, v2: Vec3): Vec3 =
     new Vec3(
@@ -85,18 +87,45 @@ object Vec3 {
       v1(2) * v2(0) - v1(0) * v2(2),
       v1(0) * v2(1) - v1(1) * v2(0)
   )
+
+  def unitVector(v: Vec3): Vec3 = v / v.length()
 }
+
+class Ray(val A: Vec3, val B: Vec3) {
+  def origin(): Vec3 = A
+  def direction(): Vec3 = B
+  def pointAtParameter(t: Float) = A + (t * B)
+}
+
+object Ray {
+  def apply(A: Vec3, B: Vec3): Ray = new Ray(A, B)
+}
+
+import Vec3._
 
 object RayTracing extends App {
   val nx = 200
   val ny = 100
   print(s"P3\n${nx} ${ny}\n255\n")
+  val lowerLeftCorner = Vec3(-2.0f, -1.0f, -1.0f)
+  val horizontal = Vec3(4.0f, 0.0f, 0.0f)
+  val vertical = Vec3(0.0f, 2.0f, 0.0f)
+  val origin = Vec3(0.0f, 0.0f, 0.0f)
   for (j <- (ny - 1) to 0 by -1;
        i <- 0 until nx) {
-    val col = new Vec3(i.toFloat / nx.toFloat, j.toFloat / ny.toFloat, 0.2f)
+    val u = i.toFloat / nx.toFloat
+    val v = j.toFloat / ny.toFloat
+    val ray = Ray(origin, lowerLeftCorner + (u * horizontal) + (v * vertical))
+    val col = color(ray)
     val ir = (255.99f * col(0)).toInt
     val ig = (255.99f * col(1)).toInt
     val ib = (255.99f * col(2)).toInt
     print(s"${ir} ${ig} ${ib}\n")
+  }
+
+  def color(r: Ray): Vec3 = {
+    val unitDirection = unitVector(r.direction())
+    val t = 0.5f * (unitDirection.y + 1.0f)
+    (1.0f - t) * Vec3(1.0f, 1.0f, 1.0f) + t * Vec3(0.5f, 0.7f, 1.0f)
   }
 }
