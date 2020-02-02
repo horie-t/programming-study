718f849bc137433730adea88f75d38d179aafec7
HORIE Tetsuya <horie-t@users.noreply.github.com>
2020-02-02 20:17:04 +0900

    ScalaFXを使う

---
+++ slam/src/main/scala/Slam.scala
@@ -0,0 +1,21 @@
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.canvas.Canvas
+import scalafx.scene.Scene
+import scalafx.scene.paint.Color
+
+object ScalaFXHelloCanvas extends JFXApp {
+  val canvas = new Canvas(800, 800)
+  val gc = canvas.graphicsContext2D
+
+  gc.stroke = Color.Black
+  gc.lineWidth = 1
+  gc.strokeLine(40, 40, 760, 760)
+
+  stage = new PrimaryStage {
+    title = "ScalaFX HelloCanvas"
+    scene = new Scene {
+      content = canvas
+    }
+  }
+}
