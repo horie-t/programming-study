89c156e1cb2d9851d3e7a788af573f4cc5503499
HORIE Tetsuya <horie-t@users.noreply.github.com>
2020-02-04 22:08:06 +0900

    SLAM: 画面の枠線を作成

--- slam/src/main/scala/Slam.scala
+++ slam/src/main/scala/Slam.scala
@@ -5,12 +5,21 @@ import scalafx.scene.Scene
 import scalafx.scene.paint.Color
 
 object ScalaFXHelloCanvas extends JFXApp {
-  val canvas = new Canvas(800, 800)
+  val canvas = new Canvas(700, 700)
   val gc = canvas.graphicsContext2D
 
+  // 左下が負の象限になるようにする。
+  gc.scale(50, 50)
+  gc.translate(7, 7)
+  gc.transform(1, 0, 0, -1, 0, 0)
+
+  // 枠を描く
   gc.stroke = Color.Black
-  gc.lineWidth = 1
-  gc.strokeLine(40, 40, 760, 760)
+  gc.lineWidth = 0.02
+  gc.strokeLine(-6, -6, 6, -6)
+  gc.strokeLine(-6, -6, -6, 6)
+  gc.strokeLine(-6, 6, 6, 6)
+  gc.strokeLine(6, -6, 6, 6)
 
   stage = new PrimaryStage {
     title = "ScalaFX HelloCanvas"
