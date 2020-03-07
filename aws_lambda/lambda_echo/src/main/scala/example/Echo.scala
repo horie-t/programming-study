12b3dc0fd7042c5ade22742824ac6150dad08a05
HORIE Tetsuya <horie-t@users.noreply.github.com>
2020-03-07 21:24:28 +0900

    AWS Lambda: POSTリクエストのbodyをレスポンスのbodyにする

--- aws_lambda/lambda_echo/src/main/scala/example/Echo.scala
+++ aws_lambda/lambda_echo/src/main/scala/example/Echo.scala
@@ -1,13 +1,33 @@
 package example
 
+import java.util.Base64
+
 import scala.collection.JavaConverters._
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 
+import scala.beans.BeanProperty
 
-trait EchoBase {
-  def echo(phrase: String, context: Context) = {
-    println(phrase)
+class Request {
+  @BeanProperty
+  var body: String = _
+}
+
+class Response(bodyJson: String) {
+  @BeanProperty
+  val statusCode = 200
+
+  @BeanProperty
+  val headers: Object = new Object
+
+  @BeanProperty
+  var body: String = bodyJson
+}
+
+trait EchoBase extends RequestHandler[Request, Response]{
+  override def handleRequest(request: Request, context: Context) = {
+    val decodedBody = new String(Base64.getDecoder.decode(request.body))
+    new Response(decodedBody)
   }
 }
 
