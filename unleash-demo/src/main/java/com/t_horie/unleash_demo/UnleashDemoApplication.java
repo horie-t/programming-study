cd8d5b8c119a6b0f70b252157ca43463723769cf
HORIE Tetsuya <horie-t@users.noreply.github.com>
2024-11-30 12:12:40 +0900

    Unleashデモプロジェクトの初期コミット
    Spring initializrで初期構成を作成しただけ

---
+++ unleash-demo/src/main/java/com/t_horie/unleash_demo/UnleashDemoApplication.java
@@ -0,0 +1,13 @@
+package com.t_horie.unleash_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnleashDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnleashDemoApplication.class, args);
+	}
+
+}
---
+++ unleash-demo/src/test/java/com/t_horie/unleash_demo/UnleashDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t_horie.unleash_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnleashDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
